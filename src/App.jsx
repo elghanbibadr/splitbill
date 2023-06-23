6f971fae4c3a0a40999086c689b5408d79e4e3ec
badr ghanbi <bghanbi50@gmail.com>
2023-06-23 15:08:37 +0100

    create the first filed of the form

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,20 @@ function App() {
 
   return (
     <>
-      <div className='bg-[#fefefe] text-[#1a1a1a] w-fit p-3 rounded-md'>
-        {FriendsData.map(({ image, name, statusWithThisFriend }) => {
-          return <Friend image={image} name={name} statusWithThisFriend={statusWithThisFriend} />
-        })}
+      <div className='grid grid-cols-2 p-10'>
+        <div className='bg-[#fefefe] text-[#1a1a1a] w-fit p-3 rounded-md'>
+          {FriendsData.map(({ image, name, statusWithThisFriend }) => {
+            return <Friend image={image} name={name} statusWithThisFriend={statusWithThisFriend} />
+          })}
+        </div>
+        {/* form section */}
+        <form className='bg-[#fefefe] text-[#1a1a1a] w-fit p-6 rounded-md'>
+          <h1 className='text-xl'>SPLIT A BILL WITH ANTHONY</h1>
+          <div className='mt-3'>
+            <label htmlFor="bill value">Bill value </label>
+            <input className='bg-transparent border mx-3 border-black rounded-md' id='bill value' type="number" />
+          </div>
+        </form>
       </div>
 
     </>
